f0483cda8fb69a9960343c92d90da175f0593eda
Eder Andres Chavez Ruiz <eder.ac.ruiz@gmail.com>
2017-09-24 22:03:25 -0500

    Added first part of module for render request to login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //import modules require from node
 const express = require('express');
 const bodyParser = require('body-parser');
+const hbs = require('express-handlebars');
 
 //initializing express application
 const app = express();
@@ -11,6 +12,18 @@ const api_rest = require('./routes');//it is not necessary indicate index.js by 
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+app.engine('.hbs', hbs({
+
+	defaultLayout: 'default',
+	extname: '.hbs'
+
+}));
+app.set('view engine', '.hbs');
+
+//set to use and handle routes requested
 app.use('/api', api_rest);
+app.get('/login', (req, res) => {
+	res.render('login');//look for a .hbs on views folder level, because of set on app.engine 
+});
 
 module.exports = app;
